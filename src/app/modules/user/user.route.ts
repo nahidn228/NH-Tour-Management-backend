e0ec38f079bd96d053f97bd7161eb599c3b988c1
nahidn228 <nahidn228@gmail.com>
2025-07-15 00:09:31 +0600

    add zodschema in update route

--- src/app/modules/user/user.route.ts
+++ src/app/modules/user/user.route.ts
@@ -1,11 +1,10 @@
 import { userController } from "./user.controller";
-import { createUserZodSchema } from "./user.validation";
+import { createUserZodSchema, updateUserZodSchema } from "./user.validation";
 import { validateRequest } from "../../middleware/validateRequest";
 
 import { checkAuth } from "../../middleware/checkAuth";
 import { Router } from "express";
 import { Role } from "./user.interface";
-
 
 const router = Router();
 
@@ -22,6 +21,7 @@ router.get(
 );
 router.patch(
   "/:id",
+  validateRequest(updateUserZodSchema),
   checkAuth(...Object.values(Role)),
   userController.updateUser
 );
