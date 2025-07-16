0c94c866d4fdf0a375bad360f23fef0d1cc57c6d
nahidn228 <nahidn228@gmail.com>
2025-07-16 09:41:28 +0600

    Configuring Passport JS for Backend and Google Authentication Configuration

--- src/app.ts
+++ src/app.ts
@@ -7,8 +7,21 @@ import { router } from "./app/routes";
 import { globalErrorHandler } from "./app/middleware/globalErrorHandler";
 import notFound from "./app/middleware/notFound";
 import cookieParser from "cookie-parser";
+import passport from "passport";
+import expressSession from "express-session";
 
 const app = express();
+
+app.use(
+  expressSession({
+    secret: "your secret",
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
 app.use(express.json());
 app.use(cors());
 app.use(cookieParser());
--- src/app/config/env.ts
+++ src/app/config/env.ts
@@ -13,6 +13,12 @@ interface IEnvConfig {
   BCRYPT_SALT_ROUND: string;
   SUPER_ADMIN_EMAIL: string;
   SUPER_ADMIN_PASSWORD: string;
+
+  GOOGLE_CLIENT_SECRET: string;
+  GOOGLE_CLIENT_ID: string;
+  GOOGLE_CALLBACK_URL: string;
+  EXPRESS_SESSION_SECRET: string;
+  FRONTEND_URL: string;
 }
 
 const loadEnvVariables = (): IEnvConfig => {
@@ -27,6 +33,11 @@ const loadEnvVariables = (): IEnvConfig => {
     "BCRYPT_SALT_ROUND",
     "SUPER_ADMIN_EMAIL",
     "SUPER_ADMIN_PASSWORD",
+    "GOOGLE_CLIENT_SECRET",
+    "GOOGLE_CLIENT_ID",
+    "GOOGLE_CALLBACK_URL",
+    "EXPRESS_SESSION_SECRET",
+    "FRONTEND_URL",
   ];
   requireEnvVariables.forEach((key) => {
     if (!process.env[key]) {
@@ -45,6 +56,12 @@ const loadEnvVariables = (): IEnvConfig => {
     BCRYPT_SALT_ROUND: process.env.BCRYPT_SALT_ROUND as string,
     SUPER_ADMIN_EMAIL: process.env.SUPER_ADMIN_EMAIL as string,
     SUPER_ADMIN_PASSWORD: process.env.SUPER_ADMIN_PASSWORD as string,
+
+    GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET as string,
+    GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID as string,
+    GOOGLE_CALLBACK_URL: process.env.GOOGLE_CALLBACK_URL as string,
+    EXPRESS_SESSION_SECRET: process.env.EXPRESS_SESSION_SECRET as string,
+    FRONTEND_URL: process.env.FRONTEND_URL as string,
   };
 };
 
