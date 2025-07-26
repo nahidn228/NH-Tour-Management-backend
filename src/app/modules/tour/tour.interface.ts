b8de92e28c167d4b7ee9d797f5f3685aee6a3328
nahidn228 <nahidn228@gmail.com>
2025-07-26 11:50:36 +0600

    Create model for Tour and Tour Types

--- src/app/modules/tour/tour.interface.ts
+++ src/app/modules/tour/tour.interface.ts
@@ -1,20 +1,23 @@
 import { Types } from "mongoose";
 
+export interface ITourType {
+  name: string;
+}
 export interface ITour {
   title: string;
   slug: string;
   description?: string;
   images?: string[];
   location?: string;
-  coastFrom?: number;
+  costFrom?: number;
   startDate?: Date;
   endDate?: Date;
   included?: string[];
   excluded?: string[];
   amenities?: string[];
-  tourPlan?: string;
+  tourPlan?: string[];
   maxGuest?: number;
   minAge?: number;
   division: Types.ObjectId;
-  tourTypes: Types.ObjectId;
+  tourType: Types.ObjectId;
 }
